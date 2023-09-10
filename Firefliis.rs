d0e8828100d7aa6f91232108e68a4982b5905f53
Firefliis <144391890+Firefliis@users.noreply.github.com>
2023-09-10 01:40:04 -0400

    Update Firefliis.rs

--- Firefliis.rs
+++ Firefliis.rs
@@ -1,1 +1,52 @@
+struct UserProfile {
+    username: String,
+    age: u32,
+    // Add more fields as needed
+}
+table! {
+    users {
+        id -> Integer,
+        username -> Text,
+        age -> Integer,
+        // Define more columns as needed
+    }
+}
+#[derive(Queryable, Insertable)]
+#[table_name = "users"]
+struct User {
+    id: i32,
+    username: String,
+    age: i32,
+}
 
+// Implement functions to perform CRUD operations
+fn create_user(connection: &PgConnection, new_user: User) -> QueryResult<usize> {
+    diesel::insert_into(users::table)
+        .values(&new_user)
+        .execute(connection)
+}
+
+fn get_user_by_id(connection: &PgConnection, user_id: i32) -> QueryResult<User> {
+    users::table.find(user_id).get_result(connection)
+}
+#[macro_use] extern crate rocket;
+
+#[get("/user/<id>")]
+fn get_user(id: i32) -> String {
+    // Implement logic to fetch user by ID and return JSON response
+    format!("User ID: {}", id)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![get_user])
+}
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Your code here
+    let result = some_function();
+    if let Err(err) = result {
+        eprintln!("Error: {}", err);
+        // Handle the error
+    }
+    Ok(())
+}
